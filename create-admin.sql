fb6365bb948f65b4583ebbe9e0ff3c17fea60239
Naveed Ahmed <162731153+Naveed-CoLab@users.noreply.github.com>
2025-05-01 21:28:15 +0500

    Update create-admin.sql

--- create-admin.sql
+++ create-admin.sql
@@ -5,5 +5,5 @@ ON CONFLICT (username) DO NOTHING;
 
 -- Create regular user for testing
 INSERT INTO users (username, password, email, role, is_active)
-VALUES ('user', '$2b$10$zXlQUmxRxd9oO.jkl1bzR.wAtS0oWUlg2W5MJp/P4vIZYFIlM1vwi', 'user@example.com', 'user', true)
+VALUES ('user', '$2b$10$zXlQUmxRxd9oO.jkl1bzR.wAtS0oWUlg2W5MJp/P4vIZYFIlM1vwi', 'user@tripsage.com', 'user', true)
 ON CONFLICT (username) DO NOTHING;
